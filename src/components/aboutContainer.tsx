9a707770417cdf20c9c1561e64977ac40ae62c7d
Rashmi R <rashmir3107@gmail.com>
2023-12-28 11:45:52 +0530

    content and color changes

--- src/components/aboutContainer.tsx
+++ src/components/aboutContainer.tsx
@@ -13,7 +13,7 @@ import manjuContent from "./constants/manjuContent";
 import { useState } from "react";
 
 function AboutContainer() {
-  const content = `Manbru Associates is a leading firm that specializes in providing comprehensive legal, Chartered Accountant (CA), and Company Secretary (CS) services under one roof. With a team of highly skilled professionals, including experienced lawyers, certified accountants, and company secretaries, we offer integrated solutions to meet the diverse needs of our clients. Our firm is committed to delivering exceptional services that are tailored to the unique requirements of each client, whether they are individuals, small businesses, or large corporations. We combine our legal expertise with financial acumen and corporate governance knowledge to provide comprehensive solutions for legal compliance, taxation matters, company formations, regulatory filings, and more. At Manbru Associates, we strive to be a trusted partner for our clients, ensuring their legal, financial, and regulatory needs are met efficiently and effectively.
+  const content = `Manbru Associates is a leading firm that specializes in providing comprehensive Legal & Chartered Accountancy (CA) services under one roof. With a team of highly skilled professionals, including experienced lawyers, certified accountants, and company secretaries, we offer integrated solutions to meet the diverse needs of our clients. Our firm is committed to delivering exceptional services that are tailored to the unique requirements of each client, whether they are individuals, small businesses, or large corporations. We combine our legal expertise with financial acumen and corporate governance knowledge to provide comprehensive solutions for legal compliance, taxation matters, company formations, regulatory filings, and more. At Manbru Associates, we strive to be a trusted partner for our clients, ensuring their legal, financial, and regulatory needs are met efficiently and effectively.
   `;
 
   const [open, setOpen] = useState(false);
@@ -102,13 +102,13 @@ function AboutContainer() {
         <Container>
           <Box sx={{ display: { xs: "none", lg: "block" } }}>
             <AboutHorizontalContainer
-              name="BRUNDA RAO"
+              name="Brunda Rao"
               content={brundaContent}
               imageUrl="./brunda.JPG"
               right={true}
             ></AboutHorizontalContainer>
             <AboutHorizontalContainer
-              name="C A Manjunath P S"
+              name="CA. Manjunatha P. S."
               content={manjuContent}
               imageUrl="./manju.jpg"
               right={false}
--- src/components/constants/brundaContent.tsx
+++ src/components/constants/brundaContent.tsx
@@ -1,7 +1,5 @@
 const brundaContent = `The dynamic founder of Manbru Associates, is a distinguished legal 
-professional with over 20 years of diverse expertise. Holding a post-graduate degree 
-in law from the renowned National Law School, Bangalore, Brunda has honed her skills 
-in various specialized areas. Her broad range of legal proficiency encompasses Intellectual Property Rights (IPR),
+professional with over 20 years of diverse expertise. Her broad range of legal proficiency encompasses Intellectual Property Rights (IPR),
 Food laws, Pharmaceutical regulations, E-commerce, Real estate, Labor and employment laws, Contracts and Clinical trials.
 With her profound knowledge and a proven track record, Brunda has guided numerous clients through complex legal challenges, 
 providing strategic counsel and innovative solutions. At the helm of Manbru Associates, she leads a team of talented 
